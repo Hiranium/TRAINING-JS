e17a5159d1e85f0ace6c18486965df5de5c4e248
Hiranium <hirenrajdadhwal@gmail.com>
2025-12-28 15:17:07 +0530

    constant and variables

---
+++ basics/vid2.js
@@ -0,0 +1,20 @@
+const accountId = 47220
+let accountEmail = "hiren@google.com"
+var accountPassword =  "46290"
+accountCity = "Jaipur"   
+let accountState;
+
+// accountID = 2 // not allowed
+
+accountEmail = "hd@google.com"
+accountPassword = "6363559"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+comment space -> prefer not use  var
+because of issue in block scope and functional scope 
+*/
