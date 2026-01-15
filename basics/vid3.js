3dce71c6b14de8e74dc31a766c7cb39e88629349
Hiranium <hirenrajdadhwal@gmail.com>
2026-01-16 01:36:07 +0530

    datatypes

--- basics/vid3.js
+++ basics/vid3.js
@@ -5,6 +5,8 @@
 // console.log(3
 // + 3) // code readability should be high
 
+
+ //*********************************datatypes *********************// */
 console.log("hiren");
 let name = "hiren" 
 let age = 21
