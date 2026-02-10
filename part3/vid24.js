c0ae061c692b581d86ff489c73ba7045bd15dbf7
Hiranium <hirenrajdadhwal@gmail.com>
2026-02-10 14:27:51 +0530

    control flow 1

---
+++ part3/vid24.js
@@ -0,0 +1,58 @@
+// CONTROL FLOW//LOGIC CONTROL
+//if
+// const isUserloggedIn = true
+// const temperature = 60
+
+// if ( temperature < 50 ) {
+    // console.log("less than 50");
+// } else {
+    // console.log("temperature is greater than 50"); 
+// } 
+
+// <,>, <=, >=, ==, ===, !=, !== 
+
+// const score  = 200
+// if (score > 100){
+    // const power =  "fly"
+    // console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test") , console.log("test2") , console.log("test3")    // dont do this ever
+
+// nesting if statements
+
+// if (balance < 500) {
+    // console.log("balance is less than 500");
+// } else if (balance < 750 ) {
+    // console.log("balance is less than 750");
+// } else if (balance < 900) {
+    // console.log("balance is less than 900");
+// } else {
+    // console.log("less than 1200");
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail  = true
+
+// if (userLoggedIn && debitCard && 2==2) {
+    // console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+    // console.log("User logged in");
+// }
+
+
+
+
+
+
+
+
