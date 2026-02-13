367d7394cc742a11ccde040a9cce101c54d3f494
Hiranium <hirenrajdadhwal@gmail.com>
2026-02-14 01:40:11 +0530

    Array loops 2

---
+++ part3/vid27_2.js
@@ -0,0 +1,30 @@
+// for in loop
+
+
+
+// const myObject = {
+//     js: 'javaScript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     Swift: 'swift by apple'
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);    
+// }
+
+// const programming = ["js","rb","py","java","cpp"];
+
+// for (const key in programming) {
+//     console.log(`${key} number is for ${programming[key]} programming language`);
+// }
+// const map = new Map()
+// map.set('IN', 'India')  
+// map.set('USA', 'United States of America') 
+// map.set('Fr', 'France')
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+
